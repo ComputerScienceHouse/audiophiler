733b6559ccfe795b487822cd8681cb92a9e2a11e
Stephen Greene <sgreene570@gmail.com>
2017-04-01 21:34:50 -0400

    fixed acl

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import host
 def main():
     s3 = boto3.resource(service_name="s3", endpoint_url=host.s3_url)
     data = open("test.mp3", "rb")
-    s3.Bucket("audiophiler").put_object(Key="testsong.mp3", Body=data)
+    s3.Bucket("audiophiler").put_object(Key="testsong.mp3", Body=data, ACL="public-read")
 
 
 if __name__ == "__main__":
