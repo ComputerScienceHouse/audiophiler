479f10a3ceab71468393adeeb8049eb642162ca0
Stephen Greene <sgreene570@gmail.com>
2017-04-07 19:13:22 -0400

    added live file menu on main

--- audiophiler/app.py
+++ audiophiler/app.py
@@ -6,17 +6,28 @@ import boto3
 import host
 app = Flask(__name__)
 
+BUCKET_NAME = "audiophiler"
+
 @app.route("/", methods=["POST", "GET"])
 def test():
-    return render_template("main.html")
+    s3 = get_resource()
+    bucket = get_bucket()
+    s3_files = bucket.objects.all()
+    return render_template("main.html", s3_files=s3_files)
 
 
 @app.route("/upload", methods=["POST", "GET"])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-        s3 = boto3.resource(service_name="s3", endpoint_url=host.s3_url)
-        s3.Bucket("audiophiler").put_object(Key=secure_filename(f.filename), Body=f, ACL="public-read")
+        bucket = get_bucket()
+        bucket.put_object(Key=secure_filename(f.filename), Body=f, ACL="public-read")
     return render_template("upload.html")
 
 
+def get_resource():
+    return boto3.resource(service_name="s3", endpoint_url=host.s3_url)
+
+
+def get_bucket():
+    return get_resource().Bucket(BUCKET_NAME)
