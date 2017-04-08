bd5349326e2cce37b5f95a1a2c8f8222d656f16a
Stephen Greene <sgreene570@gmail.com>
2017-04-07 20:53:57 -0400

    implemented html audio playback

--- audiophiler/app.py
+++ audiophiler/app.py
@@ -3,17 +3,21 @@ from flask import render_template
 from flask import request
 from werkzeug.utils import secure_filename
 import boto3
-import host
+import host         # host.py file containing s3 url
 app = Flask(__name__)
 
+
 BUCKET_NAME = "audiophiler"
+
 
 @app.route("/", methods=["POST", "GET"])
 def test():
     s3 = get_resource()
     bucket = get_bucket()
     s3_files = bucket.objects.all()
-    return render_template("main.html", s3_files=s3_files)
+    for s3_file in s3_files:
+        print(get_file_url(s3_file.key))
+    return render_template("main.html", s3_files=s3_files, get_file_url=get_file_url)
 
 
 @app.route("/upload", methods=["POST", "GET"])
@@ -23,6 +27,10 @@ def upload():
         bucket = get_bucket()
         bucket.put_object(Key=secure_filename(f.filename), Body=f, ACL="public-read")
     return render_template("upload.html")
+
+
+def get_file_url(s3_key):
+    return host.s3_url + "/" + BUCKET_NAME + "/" + s3_key
 
 
 def get_resource():
