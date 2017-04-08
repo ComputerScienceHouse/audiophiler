e793928a789e4c4fb1997109d9bbeccdbcd9dcd1
Stephen Greene <sgreene570@gmail.com>
2017-04-07 23:14:45 -0400

    hiding static files with send_from_directory

--- audiophiler/app.py
+++ audiophiler/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import send_from_directory
 from werkzeug.utils import secure_filename
+import os, sys
 import boto3
+import tempfile
 import host         # host.py file containing s3 url
 app = Flask(__name__)
 
@@ -11,13 +14,11 @@ BUCKET_NAME = "audiophiler"
 
 
 @app.route("/", methods=["POST", "GET"])
-def test():
+def home():
     s3 = get_resource()
     bucket = get_bucket()
     s3_files = bucket.objects.all()
-    for s3_file in s3_files:
-        print(get_file_url(s3_file.key))
-    return render_template("main.html", s3_files=s3_files, get_file_url=get_file_url)
+    return render_template("main.html", s3_files=s3_files, get_file=get_file)
 
 
 @app.route("/upload", methods=["POST", "GET"])
@@ -25,12 +26,16 @@ def upload():
     if request.method == 'POST':
         f = request.files['file']
         bucket = get_bucket()
-        bucket.put_object(Key=secure_filename(f.filename), Body=f, ACL="public-read")
+        bucket.put_object(Key=secure_filename(f.filename), Body=f)
     return render_template("upload.html")
 
 
-def get_file_url(s3_key):
-    return host.s3_url + "/" + BUCKET_NAME + "/" + s3_key
+@app.route("/uploads/<path:filename>")
+def get_file(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    s3 = get_resource()
+    s3.meta.client.download_file(BUCKET_NAME, filename, os.path.join(root_dir, "/temp/", filename))
+    return send_from_directory(os.path.join(root_dir, "/temp/"), filename)
 
 
 def get_resource():
