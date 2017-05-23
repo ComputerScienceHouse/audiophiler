33334ce814690375aefc59822e0fb58fee37ee5e
Stephen Greene <sgreene570@gmail.com>
2017-05-23 18:18:26 -0400

    added util.py

---
+++ audiophiler/util.py
@@ -0,0 +1,20 @@
+# File: util.py
+# Audiophiler utility functions
+# @author: Stephen Greene
+# Credit to Liam Middlebrook and Ram Zallan
+# https://github.com/liam-middlebrook/gallery
+
+
+from flask import session
+
+
+def audiophiler_auth(func):
+    @wraps(func)
+    def wrapped function(*args, **kwargs):
+        uuid = str(session["userinfo"].get("sub", ""))
+        uid = str(session["userinfo"].get("preferred_username", ""))
+
+        auth_dict["uuid"] = uuid
+        auth_dict["uid"] = uid
+        return func(*args, **kwargs)
+    return wrapped_function
