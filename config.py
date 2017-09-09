c6a7eb735f8055f9a034b2c500d504e2cbe3f3e9
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-09-09 18:11:02 -0400

    Add support for CSH: Plug!

--- config.py
+++ config.py
@@ -32,4 +32,4 @@ LDAP_BIND_DN = os.getenv("LDAP_BIND_DN", default="cn=audiophiler,ou=Apps,dc=csh,
 LDAP_BIND_PW = os.getenv("LDAP_BIND_PW", default=None)
 
 
-
+PLUG_SUPPORT = os.environ.get('PLUG_ENABLED', 'False') == 'True'
